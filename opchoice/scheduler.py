c5dd8b184f71e41a4e11191cf0e758bf1f5e28fe
iacopo breschi <iacopobreschi@gmail.com>
2017-10-20 01:23:31 +0200

    [Refactoring] corrected model dependancy

--- opchoice/scheduler.py
+++ opchoice/scheduler.py
@@ -14,12 +14,10 @@ from pygraph.algorithms.minmax import shortest_path
 from pygraph.algorithms.minmax import shortest_path_bellman_ford
 from pygraph.classes.digraph import digraph
 
-from .model import Activity
-from .model import TimeInterval
-from .model import overlap
-from .model import same_day
-from .model import same_week
-from .utils import ProgressBar
+#from .model import Activity
+from .TimeInterval import *
+
+#from .utils import ProgressBar
 
 
 # Activities List 
@@ -55,180 +53,202 @@ from .utils import ProgressBar
 #############################################    
 # decisor = GraphActivitiesDecisor(activity_list, busy_list, 1)
 # print decisor.decideActivities()
-class GraphActivitiesDecisor():
-    def __init__(self, activity_list):
-        self.activity_list = list(activity_list)
-            
-    def __makeGraph(self, l_activities):
-        
-        my_graph = digraph()
-        # Nodes
-        l_activities.insert(0, Activity('__s__', TimeInterval(0 , 0) , 0))
-        l_activities.append(Activity('__t__', TimeInterval(sys.maxint , sys.maxint) , 0))    
-        nodes = range(len(l_activities))
-        my_graph.add_nodes(nodes)
-
-        # Edges
-        for i in nodes:
-            for j in nodes:
-                a_i = l_activities[i]
-                a_j = l_activities[j]
-                # 1 they are not the same node 
-                # 2 
-                if (i != j) and a_i.interval.end < a_j.interval.start:
-                    # add one arc from 
-                    my_graph.add_edge((i, j), wt=-a_i.value)
-        return my_graph
-       
-    def __hasDuplicates(self, l_activities):
-        d_map = self.__duplicates_map(l_activities)
-        for l_elem in d_map.values():
-            if len(l_elem) > 1:
-                return True
-        return False
-       
-    def __duplicates_map(self, l_activities):
-        names = {}
-        for i in l_activities:
-            if names.has_key(i.name):
-                names[i.name].append(i)
-            else:
-                names[i.name] = [i]
-        return names
-#         for val in names.items():
-#             if val >1 :
+# class GraphActivitiesDecisor():
+#     def __init__(self, activity_list):
+#         self.activity_list = list(activity_list)
+#             
+#     def __makeGraph(self, l_activities):
+#         
+#         my_graph = digraph()
+#         # Nodes
+#         l_activities.insert(0, Activity('__s__', TimeInterval(0 , 0) , 0))
+#         l_activities.append(Activity('__t__', TimeInterval(sys.maxint , sys.maxint) , 0))    
+#         nodes = range(len(l_activities))
+#         my_graph.add_nodes(nodes)
+# 
+#         # Edges
+#         for i in nodes:
+#             for j in nodes:
+#                 a_i = l_activities[i]
+#                 a_j = l_activities[j]
+#                 # 1 they are not the same node 
+#                 # 2 
+#                 if (i != j) and a_i.interval.end < a_j.interval.start:
+#                     # add one arc from 
+#                     my_graph.add_edge((i, j), wt=-a_i.value)
+#         return my_graph
+#        
+#     def __hasDuplicates(self, l_activities):
+#         d_map = self.__duplicates_map(l_activities)
+#         for l_elem in d_map.values():
+#             if len(l_elem) > 1:
 #                 return True
 #         return False
+#        
+#     def __duplicates_map(self, l_activities):
+#         names = {}
+#         for i in l_activities:
+#             if names.has_key(i.name):
+#                 names[i.name].append(i)
+#             else:
+#                 names[i.name] = [i]
+#         return names
+# #         for val in names.items():
+# #             if val >1 :
+# #                 return True
+# #         return False
+# 
+#     def __getBestActivities(self, l_activities):
+#         my_graph = self.__makeGraph(l_activities)
+#         # spanning_tree, dist = shortest_path(my_graph, 0)
+#         spanning_tree, dist = shortest_path_bellman_ford(my_graph, 0)
+#         final_node_id = len(l_activities) - 1
+#         v = spanning_tree[final_node_id]
+#         
+#         activity_decision = []
+#         while v != None:
+#             activity_decision.append(l_activities[v])
+#             v = spanning_tree[v]
+#         # pop the __s__ node from decision
+#         activity_decision.pop()
+#         activity_decision.reverse()
+#         return activity_decision , -dist[final_node_id]
+# 
+#     def decideActivities(self):
+#         
+#         # 1: separate elements that appears more than one to the single ones from the filtered list
+#         multiples_acts = []
+#         singles_acts = []
+#         
+#         duplicats_map = self.__duplicates_map(self.activity_list)
+#         for l_acts_i in duplicats_map.values():
+#             if len(l_acts_i) == 1:
+#                 singles_acts += l_acts_i
+#             else:
+#                 multiples_acts += l_acts_i
+#         
+#         print 'Tot activities #:', len(self.activity_list)
+#         print 'Multiples activity #:', len(multiples_acts)
+#         print 'Single activity #:', len(singles_acts)
+#         
+#         # 2: Get the number of activities that appears more than one time
+#         # Es: multiples_acts = [(a,2,3),(a,4,3),(a,5,3),(b,2,3),(b,4,3),(c,5,3),(c,2,3),(c,4,3)] 
+#         # num_diff_act == 3
+#         list_names = []
+#         for activity in multiples_acts:
+#             list_names.append(activity.name)
+#         num_diff_act = len(list(set(list_names)))
+#         print 'Number of activities that are repeated', num_diff_act
+# 
+#         pool = tuple(multiples_acts)
+#         n = len(pool)
+#         r = num_diff_act
+#         # n!/r!/(n-r)!
+#         num_item_combinations = math.factorial(n) / math.factorial(r) / math.factorial(n - r)
+#         print 'Number of combinations', num_item_combinations, 'of length', num_diff_act
+# 
+# 
+#         # 3.a: Make all the num_diff_act length combinations of the activities 
+#         filtered_combinations = []
+#         if num_diff_act > 0:
+#             comb_iter = itertools.combinations(multiples_acts, num_diff_act)
+#             # 3.b: Remove the combinations of same activity and make it a list instead of a tuple
+#             l_cobinations = list(comb_iter)
+#             # drop_iter = itertools.dropwhile(lambda x: self.__hasDuplicates([] + list(x)), comb_iter)            
+#             # filtered_combinations = list(drop_iter)
+#             filtered_combinations = filter(lambda x: not(self.__hasDuplicates(x)), l_cobinations)
+#             print 'Number of combinations after filtering' , len(filtered_combinations)
+# #         print len(filtered_combinations)
+# #         i=0
+# #         for alist in filtered_combinations:
+# #             if self.__hasDuplicates(alist):
+# #                 i=i+1
+# #         print 'Duplicates',i
+#         # 4.a: Make a list of activities for each combination and add to it the single activities
+#         acts_list = []
+#         for comb in filtered_combinations:
+#             acts_list.append(singles_acts + list(comb))
+#         # 4.b: In case we have no multiple activities use just the single activities
+#         if len(acts_list) == 0:
+#             acts_list.append(singles_acts)
+#        
+#         # 5: compute __getBestActivities on each activity list and add the results to a list 
+#         best_activities = []
+#         best_activities_values = []
+#         
+#         print 'Number of time to execute Dijkstra', len(acts_list)
+#         
+#         if False :
+#             # copy to avoid problems
+#             new_act_list = list(acts_list[0])
+#             ex_graph = self.__makeGraph(new_act_list)
+#             print 'Number of nodes in the graph', len(ex_graph.nodes())
+#             print 'Computing:', len(acts_list), 'times a Dijkstra of O(', len(ex_graph.edges()), '+', len(ex_graph.nodes()) , ')'        
+#             
+#             my_pc_op_dur = 5.698616375191018e-06 
+#             time_estimation = my_pc_op_dur * (len(ex_graph.edges()) + len(ex_graph.nodes())) * len(acts_list)
+#             print 'Estimate time on my machine [min]:', (time_estimation) / 60.0
+#         pbar = ProgressBar(len(acts_list))
+#         for acts in acts_list:
+#             # sort ?
+#             acts.sort(key=lambda x: x.interval.start)
+#             best_acts, value = self.__getBestActivities(acts)
+#             best_activities.append(best_acts)
+#             best_activities_values.append(value)
+#             pbar.progress()
+#         
+#         # print  pbar.getTime()
+#         
+#         # 6.a: find the max from the distance list 
+#         maxValue = max(best_activities_values)
+#         #  indexMaxValue is the index of the max
+#         indexMaxValue = best_activities_values.index(maxValue)   
+#         # 6.b: return the activities that correspond to this max value
+#         return best_activities[indexMaxValue], maxValue
 
-    def __getBestActivities(self, l_activities):
-        my_graph = self.__makeGraph(l_activities)
-        # spanning_tree, dist = shortest_path(my_graph, 0)
-        spanning_tree, dist = shortest_path_bellman_ford(my_graph, 0)
-        final_node_id = len(l_activities) - 1
-        v = spanning_tree[final_node_id]
-        
-        activity_decision = []
-        while v != None:
-            activity_decision.append(l_activities[v])
-            v = spanning_tree[v]
-        # pop the __s__ node from decision
-        activity_decision.pop()
-        activity_decision.reverse()
-        return activity_decision , -dist[final_node_id]
+from . import model
 
-    def decideActivities(self):
-        
-        # 1: separate elements that appears more than one to the single ones from the filtered list
-        multiples_acts = []
-        singles_acts = []
-        
-        duplicats_map = self.__duplicates_map(self.activity_list)
-        for l_acts_i in duplicats_map.values():
-            if len(l_acts_i) == 1:
-                singles_acts += l_acts_i
-            else:
-                multiples_acts += l_acts_i
-        
-        print 'Tot activities #:', len(self.activity_list)
-        print 'Multiples activity #:', len(multiples_acts)
-        print 'Single activity #:', len(singles_acts)
-        
-        # 2: Get the number of activities that appears more than one time
-        # Es: multiples_acts = [(a,2,3),(a,4,3),(a,5,3),(b,2,3),(b,4,3),(c,5,3),(c,2,3),(c,4,3)] 
-        # num_diff_act == 3
-        list_names = []
-        for activity in multiples_acts:
-            list_names.append(activity.name)
-        num_diff_act = len(list(set(list_names)))
-        print 'Number of activities that are repeated', num_diff_act
+class SchedulerConstraints():
+    '''
+    classdocs
+    '''
+ 
+    def __init__(self):
+        '''
+        Constructor
+        '''
+        self.max_activity_by_day = sys.maxint
+        self.max_activity_by_week = sys.maxint
+        self.avoid_list = []
+        self.perform_list = []
+    
+    def add_activity_to_avoid(self, act):
+        self.avoid_list.append(act)
+    
+    def add_activity_to_perfom(self, act):
+        self.perform_list.append(act)
 
-        pool = tuple(multiples_acts)
-        n = len(pool)
-        r = num_diff_act
-        # n!/r!/(n-r)!
-        num_item_combinations = math.factorial(n) / math.factorial(r) / math.factorial(n - r)
-        print 'Number of combinations', num_item_combinations, 'of length', num_diff_act
-
-
-        # 3.a: Make all the num_diff_act length combinations of the activities 
-        filtered_combinations = []
-        if num_diff_act > 0:
-            comb_iter = itertools.combinations(multiples_acts, num_diff_act)
-            # 3.b: Remove the combinations of same activity and make it a list instead of a tuple
-            l_cobinations = list(comb_iter)
-            # drop_iter = itertools.dropwhile(lambda x: self.__hasDuplicates([] + list(x)), comb_iter)            
-            # filtered_combinations = list(drop_iter)
-            filtered_combinations = filter(lambda x: not(self.__hasDuplicates(x)), l_cobinations)
-            print 'Number of combinations after filtering' , len(filtered_combinations)
-#         print len(filtered_combinations)
-#         i=0
-#         for alist in filtered_combinations:
-#             if self.__hasDuplicates(alist):
-#                 i=i+1
-#         print 'Duplicates',i
-        # 4.a: Make a list of activities for each combination and add to it the single activities
-        acts_list = []
-        for comb in filtered_combinations:
-            acts_list.append(singles_acts + list(comb))
-        # 4.b: In case we have no multiple activities use just the single activities
-        if len(acts_list) == 0:
-            acts_list.append(singles_acts)
-       
-        # 5: compute __getBestActivities on each activity list and add the results to a list 
-        best_activities = []
-        best_activities_values = []
-        
-        print 'Number of time to execute Dijkstra', len(acts_list)
-        
-        if False :
-            # copy to avoid problems
-            new_act_list = list(acts_list[0])
-            ex_graph = self.__makeGraph(new_act_list)
-            print 'Number of nodes in the graph', len(ex_graph.nodes())
-            print 'Computing:', len(acts_list), 'times a Dijkstra of O(', len(ex_graph.edges()), '+', len(ex_graph.nodes()) , ')'        
-            
-            my_pc_op_dur = 5.698616375191018e-06 
-            time_estimation = my_pc_op_dur * (len(ex_graph.edges()) + len(ex_graph.nodes())) * len(acts_list)
-            print 'Estimate time on my machine [min]:', (time_estimation) / 60.0
-        pbar = ProgressBar(len(acts_list))
-        for acts in acts_list:
-            # sort ?
-            acts.sort(key=lambda x: x.interval.start)
-            best_acts, value = self.__getBestActivities(acts)
-            best_activities.append(best_acts)
-            best_activities_values.append(value)
-            pbar.progress()
-        
-        # print  pbar.getTime()
-        
-        # 6.a: find the max from the distance list 
-        maxValue = max(best_activities_values)
-        #  indexMaxValue is the index of the max
-        indexMaxValue = best_activities_values.index(maxValue)   
-        # 6.b: return the activities that correspond to this max value
-        return best_activities[indexMaxValue], maxValue
-
-
-class PLActivitiesDecisor():
-    def __init__(self, activity_list, max_film_for_day):
-        self.activity_list = list(activity_list)
+class Scheduler():
+    def __init__(self, constraints):
+        self.scheduler_constraints = constraints
         self.act_LPname_map = {}
-        self.max_num_film_week = 3
-        self.max_num_film_day = max_film_for_day
+        self.activity_list = []
+#         self.max_num_film_week = 3
+#         self.max_num_film_day = max_film_for_day
         
     def __activity_overlaps(self, activity):
         i1 = activity.interval
-        act_overlaps = map(lambda x: overlap(i1, x.interval) , self.activity_list)
+        act_overlaps = map(lambda x: model.overlap(i1, x.interval) , self.activity_list)
         return act_overlaps
 
     def __activity_same_day(self, activity):
         i1 = activity.interval
-        act_same_days = map(lambda x: same_day(i1, x.interval) , self.activity_list)
+        act_same_days = map(lambda x: model.same_day(i1, x.interval) , self.activity_list)
         return act_same_days
     
     def __activity_same_week(self, activity):
         i1 = activity.interval
-        act_same_week = map(lambda x: same_week(i1, x.interval) , self.activity_list)
+        act_same_week = map(lambda x: model.same_week(i1, x.interval) , self.activity_list)
         return act_same_week
 
     def __activity_types(self, activity):
@@ -237,15 +257,16 @@ class PLActivitiesDecisor():
         activity_type[index_activity_typeName] = 1
         return activity_type
 
-    def __writeProblem(self):
-        num_activities = len(self.activity_list)
+    def __writeProblem(self, activity_list):
+        self.activity_list = activity_list
+        num_activities = len(activity_list)
         # kills order 
-        self.type_names = list(set(map(lambda x: x.name, self.activity_list)))
+        self.type_names = list(set(map(lambda x: x.name, activity_list)))
         n = num_activities
         # 1 Make a list of all the activities Rating
-        Rat = map(lambda x: x.value, self.activity_list)
+        Rat = map(lambda x: x.value, activity_list)
         # 2 Make a matrix of the overlapping between activities 
-        Overlap = map(self.__activity_overlaps, self.activity_list)
+        Overlap = map(self.__activity_overlaps, activity_list)
         # 2b Make a vector with the number of overlap for each activity
         OverlapMaxVect = []
         for i in range(n):
@@ -255,11 +276,11 @@ class PLActivitiesDecisor():
                 max_ev_in_overlap = 1
             OverlapMaxVect.append(max_ev_in_overlap)
         # 3 Make a matrix of the type of each         
-        Type = map(self.__activity_types, self.activity_list)
+        Type = map(self.__activity_types, activity_list)
         # 4 Make a matrix of same day activities
-        SameDay = map(self.__activity_same_day, self.activity_list)
+        SameDay = map(self.__activity_same_day, activity_list)
         # 5 Make a matrix of same week activities
-        SameWeek = map(self.__activity_same_week, self.activity_list)
+        SameWeek = map(self.__activity_same_week, activity_list)
         
         # Problem Variables
         # xi  == True if activity xi is decided 0 otherwise
@@ -267,7 +288,7 @@ class PLActivitiesDecisor():
         
         for i in range(n):
             name = "x_%d" % i
-            self.act_LPname_map[name] = self.activity_list[i]       
+            self.act_LPname_map[name] = activity_list[i]       
         
         # Problem Objective Function 
         self.lp_prob = pulp.LpProblem("Minmax Problem", pulp.LpMaximize)
@@ -279,14 +300,14 @@ class PLActivitiesDecisor():
         for i in range(n):
             dot_SameWeek_x = pulp.lpSum([ SameWeek[j][i] * x[j] for j in range(n) ])
             label = "SameWeek_Act_constraint_%d" % i 
-            condition = pulp.lpSum([dot_SameWeek_x]) <= self.max_num_film_week
+            condition = pulp.lpSum([dot_SameWeek_x]) <= self.scheduler_constraints.max_activity_by_week
             self.lp_prob += condition, label
 
         # Time constraints, not more than self.max_num_film_day activity each day
         for i in range(n):
             dot_SameDay_x = pulp.lpSum([ SameDay[j][i] * x[j] for j in range(n) ])
             label = "SameDay_Act_constraint_%d" % i 
-            condition = pulp.lpSum([dot_SameDay_x]) <= self.max_num_film_day
+            condition = pulp.lpSum([dot_SameDay_x]) <= self.scheduler_constraints.max_activity_by_day
             self.lp_prob += condition, label
         
         # Time constraints, no activity at the same time
@@ -308,9 +329,9 @@ class PLActivitiesDecisor():
         
         self.lp_prob.writeLP("MinmaxProblem.lp")  # optional
 
-    def decideActivities(self):
+    def make_decision(self, activities):
         
-        self.__writeProblem()
+        self.__writeProblem(activities)
         self.lp_prob.solve()
 
         print "Status:", pulp.LpStatus[self.lp_prob.status]
