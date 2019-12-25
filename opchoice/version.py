86f9ad2c72422f72df69a17228524ba86058fe4d
ibreschi <iacopobreschi@gmail.com>
2019-12-26 00:50:33 +0100

    Added the wheel setup

---
+++ opchoice/version.py
@@ -0,0 +1,10 @@
+__title__ = 'opchoice'
+__description__ = 'Use python to make good decisions!'
+__version__ = '1.0.1'
+__author__ = 'ibreschi'
+__author_email__ = 'iacopobreschi@gmail.com'
+__license__ = 'UNLICENSED'
+__project_urls__ = {
+    'Source': 'https://github.com/ibreschi/opchoice',
+    'Tracker': 'https://github.com/ibreschi/opchoice/issues',
+}
